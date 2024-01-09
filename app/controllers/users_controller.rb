3128b3675840cd0fd45b340cc6e6a3ad6899f1e2
Cristian B <cris.bahena77@gmail.com>
2024-01-09 12:44:49 -0600

    added crud to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.create(
+      name: params[:name],
+      email: params[:email],
+      password_digest: params[:password_digest]
+    )
+    render :show
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(
+      name: params[:name] || @user.name,
+      email: params[:email] || @user.email,
+      password_digest: params[:password_digest] || @user.password_digest
+    )
+    render :show
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.delete
+    render json: {message: "User deleted succesfully."}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-
+  get "users" => "users#index"
+  get "users/:id" => "users#show"
+  post "users" => "users#create"
+  patch "users/:id" => "users#update"
+  delete "users/:id" => "users#destroy"
 end
